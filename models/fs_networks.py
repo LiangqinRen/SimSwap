47046520f9ff957f59c5a6d6f189601a0005f79c
LiangqinRen <liangqinren@gmail.com>
2024-01-10 21:13:15 -0600

    support simswap gan output

--- models/fs_networks.py
+++ models/fs_networks.py
@@ -10,22 +10,24 @@ import torch.nn as nn
 class InstanceNorm(nn.Module):
     def __init__(self, epsilon=1e-8):
         """
-            @notice: avoid in-place ops.
-            https://discuss.pytorch.org/t/encounter-the-runtimeerror-one-of-the-variables-needed-for-gradient-computation-has-been-modified-by-an-inplace-operation/836/3
+        @notice: avoid in-place ops.
+        https://discuss.pytorch.org/t/encounter-the-runtimeerror-one-of-the-variables-needed-for-gradient-computation-has-been-modified-by-an-inplace-operation/836/3
         """
         super(InstanceNorm, self).__init__()
         self.epsilon = epsilon
 
     def forward(self, x):
-        x   = x - torch.mean(x, (2, 3), True)
-        tmp = torch.mul(x, x) # or x ** 2
+        x = x - torch.mean(x, (2, 3), True)
+        tmp = torch.mul(x, x)  # or x ** 2
         tmp = torch.rsqrt(torch.mean(tmp, (2, 3), True) + self.epsilon)
         return x * tmp
 
+
 class ApplyStyle(nn.Module):
     """
-        @ref: https://github.com/lernapparat/lernapparat/blob/master/style_gan/pytorch_style_gan.ipynb
+    @ref: https://github.com/lernapparat/lernapparat/blob/master/style_gan/pytorch_style_gan.ipynb
     """
+
     def __init__(self, latent_size, channels):
         super(ApplyStyle, self).__init__()
         self.linear = nn.Linear(latent_size, channels * 2)
@@ -33,10 +35,11 @@ class ApplyStyle(nn.Module):
     def forward(self, x, latent):
         style = self.linear(latent)  # style => [batch_size, n_channels*2]
         shape = [-1, 2, x.size(1), 1, 1]
-        style = style.view(shape)    # [batch_size, 2, n_channels, ...]
-        #x = x * (style[:, 0] + 1.) + style[:, 1]
-        x = x * (style[:, 0] * 1 + 1.) + style[:, 1] * 1
+        style = style.view(shape)  # [batch_size, 2, n_channels, ...]
+        # x = x * (style[:, 0] + 1.) + style[:, 1]
+        x = x * (style[:, 0] * 1 + 1.0) + style[:, 1] * 1
         return x
+
 
 class ResnetBlock_Adain(nn.Module):
     def __init__(self, dim, latent_size, padding_type, activation=nn.ReLU(True)):
@@ -44,33 +47,32 @@ class ResnetBlock_Adain(nn.Module):
 
         p = 0
         conv1 = []
-        if padding_type == 'reflect':
+        if padding_type == "reflect":
             conv1 += [nn.ReflectionPad2d(1)]
-        elif padding_type == 'replicate':
+        elif padding_type == "replicate":
             conv1 += [nn.ReplicationPad2d(1)]
-        elif padding_type == 'zero':
+        elif padding_type == "zero":
             p = 1
         else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
-        conv1 += [nn.Conv2d(dim, dim, kernel_size=3, padding = p), InstanceNorm()]
+            raise NotImplementedError("padding [%s] is not implemented" % padding_type)
+        conv1 += [nn.Conv2d(dim, dim, kernel_size=3, padding=p), InstanceNorm()]
         self.conv1 = nn.Sequential(*conv1)
         self.style1 = ApplyStyle(latent_size, dim)
         self.act1 = activation
 
         p = 0
         conv2 = []
-        if padding_type == 'reflect':
+        if padding_type == "reflect":
             conv2 += [nn.ReflectionPad2d(1)]
-        elif padding_type == 'replicate':
+        elif padding_type == "replicate":
             conv2 += [nn.ReplicationPad2d(1)]
-        elif padding_type == 'zero':
+        elif padding_type == "zero":
             p = 1
         else:
-            raise NotImplementedError('padding [%s] is not implemented' % padding_type)
+            raise NotImplementedError("padding [%s] is not implemented" % padding_type)
         conv2 += [nn.Conv2d(dim, dim, kernel_size=3, padding=p), InstanceNorm()]
         self.conv2 = nn.Sequential(*conv2)
         self.style2 = ApplyStyle(latent_size, dim)
-
 
     def forward(self, x, dlatents_in_slice):
         y = self.conv1(x)
@@ -82,59 +84,180 @@ class ResnetBlock_Adain(nn.Module):
         return out
 
 
+class Generator(nn.Module):
+    def __init__(self, input_nc, output_nc, deep=False):
+        super(Generator, self).__init__()
+        activate_layer = nn.ReLU(True)
+        norm_layer = nn.BatchNorm2d
+        self.deep = deep
+
+        self.first_layer = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(input_nc, 64, kernel_size=7, padding=0),
+            norm_layer(64),
+            activate_layer,
+        )
+
+        self.down1 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            norm_layer(128),
+            activate_layer,
+        )
+        self.down2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            norm_layer(256),
+            activate_layer,
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+            norm_layer(512),
+            activate_layer,
+        )
+        self.down4 = nn.Sequential(  # deep
+            nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+            norm_layer(512),
+            activate_layer,
+        )
+
+        self.up4 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear"),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            activate_layer,
+        )
+        self.up3 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear"),
+            nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            activate_layer,
+        )
+        self.up2 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear"),
+            nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            activate_layer,
+        )
+        self.up1 = nn.Sequential(
+            nn.Upsample(scale_factor=2, mode="bilinear"),
+            nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            activate_layer,
+        )
+        self.last_layer = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(64, output_nc, kernel_size=7, padding=0),
+            nn.Tanh(),
+        )
+
+    def forward(self, input) -> tuple[torch.tensor, torch.tensor]:
+        x = input  # 3 * 224 * 224
+
+        x = self.first_layer(x)
+        x = self.down1(x)
+        x = self.down2(x)
+        x = self.down3(x)
+
+        if self.deep:
+            x = self.down4(x)
+            x = self.up4(x)
+
+        x = self.up3(x)
+        x = self.up2(x)
+        x = self.up1(x)
+        x = self.last_layer(x)
+        x = (x + 1) / 2
+
+        return x, torch.clamp(input + x, 0, 1)
+
 
 class Generator_Adain_Upsample(nn.Module):
-    def __init__(self, input_nc, output_nc, latent_size, n_blocks=6, deep=False,
-                 norm_layer=nn.BatchNorm2d,
-                 padding_type='reflect'):
-        assert (n_blocks >= 0)
+    def __init__(
+        self,
+        input_nc,
+        output_nc,
+        latent_size,
+        n_blocks=6,
+        deep=False,
+        norm_layer=nn.BatchNorm2d,
+        padding_type="reflect",
+    ):
+        assert n_blocks >= 0
         super(Generator_Adain_Upsample, self).__init__()
         activation = nn.ReLU(True)
         self.deep = deep
 
-        self.first_layer = nn.Sequential(nn.ReflectionPad2d(3), nn.Conv2d(input_nc, 64, kernel_size=7, padding=0),
-                                         norm_layer(64), activation)
+        self.first_layer = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(input_nc, 64, kernel_size=7, padding=0),
+            norm_layer(64),
+            activation,
+        )
         ### downsample
-        self.down1 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
-                                   norm_layer(128), activation)
-        self.down2 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
-                                   norm_layer(256), activation)
-        self.down3 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
-                                   norm_layer(512), activation)
+        self.down1 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1),
+            norm_layer(128),
+            activation,
+        )
+        self.down2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1),
+            norm_layer(256),
+            activation,
+        )
+        self.down3 = nn.Sequential(
+            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1),
+            norm_layer(512),
+            activation,
+        )
         if self.deep:
-            self.down4 = nn.Sequential(nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
-                                       norm_layer(512), activation)
+            self.down4 = nn.Sequential(
+                nn.Conv2d(512, 512, kernel_size=3, stride=2, padding=1),
+                norm_layer(512),
+                activation,
+            )
 
         ### resnet blocks
         BN = []
         for i in range(n_blocks):
             BN += [
-                ResnetBlock_Adain(512, latent_size=latent_size, padding_type=padding_type, activation=activation)]
+                ResnetBlock_Adain(
+                    512,
+                    latent_size=latent_size,
+                    padding_type=padding_type,
+                    activation=activation,
+                )
+            ]
         self.BottleNeck = nn.Sequential(*BN)
 
         if self.deep:
             self.up4 = nn.Sequential(
-                nn.Upsample(scale_factor=2, mode='bilinear'),
+                nn.Upsample(scale_factor=2, mode="bilinear"),
                 nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
-                nn.BatchNorm2d(512), activation
+                nn.BatchNorm2d(512),
+                activation,
             )
         self.up3 = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode="bilinear"),
             nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256), activation
+            nn.BatchNorm2d(256),
+            activation,
         )
         self.up2 = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode="bilinear"),
             nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128), activation
+            nn.BatchNorm2d(128),
+            activation,
         )
         self.up1 = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear'),
+            nn.Upsample(scale_factor=2, mode="bilinear"),
             nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64), activation
+            nn.BatchNorm2d(64),
+            activation,
         )
-        self.last_layer = nn.Sequential(nn.ReflectionPad2d(3), nn.Conv2d(64, output_nc, kernel_size=7, padding=0),
-                                        nn.Tanh())
+        self.last_layer = nn.Sequential(
+            nn.ReflectionPad2d(3),
+            nn.Conv2d(64, output_nc, kernel_size=7, padding=0),
+            nn.Tanh(),
+        )
 
     def forward(self, input, dlatents):
         x = input  # 3*224*224
@@ -161,6 +284,7 @@ class Generator_Adain_Upsample(nn.Module):
 
         return x
 
+
 class Discriminator(nn.Module):
     def __init__(self, input_nc, norm_layer=nn.BatchNorm2d, use_sigmoid=False):
         super(Discriminator, self).__init__()
@@ -168,24 +292,28 @@ class Discriminator(nn.Module):
         kw = 4
         padw = 1
         self.down1 = nn.Sequential(
-            nn.Conv2d(input_nc, 64, kernel_size=kw, stride=2, padding=padw), nn.LeakyReLU(0.2, True)
+            nn.Conv2d(input_nc, 64, kernel_size=kw, stride=2, padding=padw),
+            nn.LeakyReLU(0.2, True),
         )
         self.down2 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=kw, stride=2, padding=padw),
-            norm_layer(128), nn.LeakyReLU(0.2, True)
+            norm_layer(128),
+            nn.LeakyReLU(0.2, True),
         )
         self.down3 = nn.Sequential(
             nn.Conv2d(128, 256, kernel_size=kw, stride=2, padding=padw),
-            norm_layer(256), nn.LeakyReLU(0.2, True)
+            norm_layer(256),
+            nn.LeakyReLU(0.2, True),
         )
         self.down4 = nn.Sequential(
             nn.Conv2d(256, 512, kernel_size=kw, stride=2, padding=padw),
-            norm_layer(512), nn.LeakyReLU(0.2, True)
+            norm_layer(512),
+            nn.LeakyReLU(0.2, True),
         )
         self.conv1 = nn.Sequential(
             nn.Conv2d(512, 512, kernel_size=kw, stride=1, padding=padw),
             norm_layer(512),
-            nn.LeakyReLU(0.2, True)
+            nn.LeakyReLU(0.2, True),
         )
 
         if use_sigmoid:
@@ -211,5 +339,5 @@ class Discriminator(nn.Module):
         out.append(x)
         x = self.conv2(x)
         out.append(x)
-        
+
         return out
--- simswap_defense.py
+++ simswap_defense.py
@@ -7,6 +7,7 @@ import torch
 
 import numpy as np
 import torch.nn as nn
+import torch.optim as optim
 import torchvision.transforms as transforms
 import PIL.Image as Image
 import torch.nn.functional as F
@@ -18,26 +19,35 @@ class SimSwapDefense(nn.Module):
 
         # all relative paths base on main.py!
         self.project_path = os.path.dirname(os.path.abspath(__file__))
+        self.dataset_path = (
+            f"{self.project_path}/crop_224/vggface2_crop_arcfacealign_224"
+        )
         self.logger = logger
 
         from options.test_options import TestOptions
         from models.models import create_model
+        from models.fs_networks import Generator, Discriminator
 
         self.opt = TestOptions().parse()
         torch.nn.Module.dump_patches = True
-        self.model = create_model(self.opt)
+
+        self.target = create_model(self.opt)
+
+        self.GAN_G = Generator(input_nc=3, output_nc=3)
+        self.GAN_D = Discriminator(input_nc=3)
 
     def _get_random_pic_path(self) -> tuple[str, str]:
-        dataset_path = f"{self.project_path}/crop_224/vggface2_crop_arcfacealign_224"
-
-        people = os.listdir(dataset_path)
+        people = os.listdir(self.dataset_path)
         people1, people2 = random.sample(people, 2)
-        path1 = random.choice(os.listdir(f"{dataset_path}/{people1}"))
-        path2 = random.choice(os.listdir(f"{dataset_path}/{people2}"))
+        path1 = random.choice(os.listdir(f"{self.dataset_path}/{people1}"))
+        path2 = random.choice(os.listdir(f"{self.dataset_path}/{people2}"))
 
         self.logger.debug(f"src: {people1}/{path1}, dst: {people2}/{path2}")
 
-        return f"{dataset_path}/{people1}/{path1}", f"{dataset_path}/{people2}/{path2}"
+        return (
+            f"{self.dataset_path}/{people1}/{path1}",
+            f"{self.dataset_path}/{people2}/{path2}",
+        )
 
     def void(self):
         # randomly select two different people and swap their face
@@ -56,15 +66,15 @@ class SimSwapDefense(nn.Module):
         img_id = img_id.cuda()
         img_att = img_att.cuda()
 
-        self.model.eval()
+        self.target.eval()
 
         img_id_downsample = F.interpolate(img_id, size=(112, 112))
-        latent_id = self.model.netArc(img_id_downsample)
+        latent_id = self.target.netArc(img_id_downsample)
         latent_id = latent_id.detach().to("cpu")
         latent_id = latent_id / np.linalg.norm(latent_id, axis=1, keepdims=True)
         latent_id = latent_id.to("cuda")
 
-        img_fake = self.model(img_id, img_att, latent_id, latent_id, True)
+        img_fake = self.target(img_id, img_att, latent_id, latent_id, True)
 
         for j in range(img_id.shape[0]):
             if j == 0:
@@ -88,3 +98,171 @@ class SimSwapDefense(nn.Module):
             (cv2.imread(img_src_path), cv2.imread(img_dst_path), output), axis=1
         )
         cv2.imwrite(save_path, output)
+
+    def _get_train_pic_path(self) -> tuple[list[str], list[str]]:
+        people = os.listdir(self.dataset_path)
+        people1, people2 = people[100], people[101]  # random.sample(people, 2)
+
+        people1_imgs = [
+            f"{self.dataset_path}/{people1}/{i}"
+            for i in os.listdir(f"{self.dataset_path}/{people1}")
+        ]
+        people2_imgs = [
+            f"{self.dataset_path}/{people2}/{i}"
+            for i in os.listdir(f"{self.dataset_path}/{people2}")
+        ]
+
+        min_count = min(len(people1_imgs), len(people2_imgs))
+
+        return people1_imgs[:min_count], people2_imgs[:min_count]
+
+    def GAN(self):
+        src_imgs, dst_imgs = self._get_train_pic_path()
+        self.logger.debug(
+            f"len(src_imgs): {len(src_imgs)}, len(dst_imgs): {len(dst_imgs)}"
+        )
+
+        epoch = 64
+        optimizer_G = optim.Adam(self.GAN_G.parameters(), lr=5e-5, betas=(0.5, 0.999))
+        scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer_G, epoch)
+
+        self.target.to("cuda").eval()
+        self.GAN_G.to("cuda").train()
+
+        l1_loss = nn.L1Loss
+        l2_loss = nn.MSELoss
+
+        for i in range(64):
+            self.logger.info(f"[Epoch {i:4}]")
+            for j in range(len(src_imgs)):
+                src_img = test_one_image.transformer_Arcface(
+                    Image.open(src_imgs[j]).convert("RGB")
+                )
+                dst_img = test_one_image.transformer(
+                    Image.open(dst_imgs[j]).convert("RGB")
+                )
+
+                img_id = src_img.view(
+                    -1, src_img.shape[0], src_img.shape[1], src_img.shape[2]
+                )
+                img_att = dst_img.view(
+                    -1, dst_img.shape[0], dst_img.shape[1], dst_img.shape[2]
+                )
+
+                img_id = img_id.cuda()
+                img_att = img_att.cuda()
+
+                img_id_downsample = F.interpolate(img_id, size=(112, 112))
+                latent_id = self.target.netArc(img_id_downsample)
+                latent_id = latent_id.detach().to("cpu")
+                latent_id = latent_id / np.linalg.norm(latent_id, axis=1, keepdims=True)
+                latent_id = latent_id.to("cuda")
+
+                # get fake img
+                fake_img = self.target(img_id, img_att, latent_id, latent_id, True)
+
+                for k in range(img_id.shape[0]):
+                    if k == 0:
+                        row1 = img_id[k]
+                        row2 = img_att[k]
+                        row3 = fake_img[k]
+                    else:
+                        row1 = torch.cat([row1, img_id[k]], dim=2)
+                        row2 = torch.cat([row2, img_att[k]], dim=2)
+                        row3 = torch.cat([row3, fake_img[k]], dim=2)
+
+                full = row3.detach()
+                full = full.permute(1, 2, 0)
+                output = full.to("cpu")
+                output = np.array(output)
+                output = output[..., ::-1]
+
+                fake_img = output * 255
+
+                # get noise
+                noise, pert_img = self.GAN_G(img_id)
+
+                for k in range(img_id.shape[0]):
+                    if k == 0:
+                        row1 = img_id[k]
+                        row2 = img_att[k]
+                        row3 = noise[k]
+                    else:
+                        row1 = torch.cat([row1, img_id[k]], dim=2)
+                        row2 = torch.cat([row2, img_att[k]], dim=2)
+                        row3 = torch.cat([row3, noise[k]], dim=2)
+
+                full = row3.detach()
+                full = full.permute(1, 2, 0)
+                output = full.to("cpu")
+                output = np.array(output)
+                output = output[..., ::-1]
+
+                noise = output * 255
+
+                # get pert_img
+                for k in range(img_id.shape[0]):
+                    if k == 0:
+                        row1 = img_id[k]
+                        row2 = img_att[k]
+                        row3 = pert_img[k]
+                    else:
+                        row1 = torch.cat([row1, img_id[k]], dim=2)
+                        row2 = torch.cat([row2, img_att[k]], dim=2)
+                        row3 = torch.cat([row3, pert_img[k]], dim=2)
+
+                full = row3.detach()
+                full = full.permute(1, 2, 0)
+                output = full.to("cpu")
+                output = np.array(output)
+                output = output[..., ::-1]
+
+                pert_img_output = output * 255
+
+                pert_img = pert_img.reshape(3, 224, 224)
+                img_id = pert_img.view(
+                    -1, pert_img.shape[0], pert_img.shape[1], pert_img.shape[2]
+                )
+
+                img_id = img_id.cuda()
+
+                img_id_downsample = F.interpolate(img_id, size=(112, 112))
+                latent_id = self.target.netArc(img_id_downsample)
+                latent_id = latent_id.detach().to("cpu")
+                latent_id = latent_id / np.linalg.norm(latent_id, axis=1, keepdims=True)
+                latent_id = latent_id.to("cuda")
+
+                fake_pert_img = self.target(img_id, img_att, latent_id, latent_id, True)
+
+                for k in range(img_id.shape[0]):
+                    if k == 0:
+                        row1 = img_id[k]
+                        row2 = img_att[k]
+                        row3 = fake_pert_img[k]
+                    else:
+                        row1 = torch.cat([row1, img_id[k]], dim=2)
+                        row2 = torch.cat([row2, img_att[k]], dim=2)
+                        row3 = torch.cat([row3, fake_pert_img[k]], dim=2)
+
+                full = row3.detach()
+                full = full.permute(1, 2, 0)
+                output = full.to("cpu")
+                output = np.array(output)
+                output = output[..., ::-1]
+
+                fake_pert_img = output * 255
+
+                output = np.concatenate(
+                    (
+                        cv2.imread(src_imgs[j]),
+                        cv2.imread(dst_imgs[j]),
+                        fake_img,
+                        noise,
+                        pert_img_output,
+                        fake_pert_img,
+                    ),
+                    axis=1,
+                )
+
+                if j == 0:
+                    cv2.imwrite(f"../output/simswap/gan_{i}_{j}.png", output)
